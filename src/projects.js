f69f30d41ad5863f28bf573949ae7af9269bd304
Dylano <167641317+dstjp@users.noreply.github.com>
2024-11-20 17:17:13 +0100

    add: added Chiru's projects

--- src/projects.js
+++ src/projects.js
@@ -6,8 +6,16 @@ import antonImg1 from "./images/anton-calculator-sc.png";
 import antonImg2 from "./images/anton-tictactoe-sc.png";
 import anitaImg1 from "./images/anita-tictactoe-sc.png";
 import anitaImg2 from "./images/anita-library-sc.png";
+import chiruImg1 from "./images/chiru-calculator-sc.png";
+import chiruImg2 from "./images/chiru-colorchanger-sc.png";
 
 const projects = [
+  {
+    title: "iPhone Calculator",
+    author: "Anton Hansson",
+    url: "https://antxhan.github.io/odin-calculator/",
+    image: antonImg1,
+  },
   {
     title: "Homepage",
     author: "Dylan St. John",
@@ -15,16 +23,10 @@ const projects = [
     image: dylanImg1,
   },
   {
-    title: "Restaurant Page",
-    author: "Dylan St. John",
-    url: "https://dstjp.github.io/odin-restaurantpage/",
-    image: dylanImg2,
-  },
-  {
-    title: "iPhone Calculator",
-    author: "Anton Hansson",
-    url: "https://antxhan.github.io/odin-calculator/",
-    image: antonImg1,
+    title: "Etch a Sketch",
+    author: "Chiranjeevi V",
+    url: "https://chiru1985.github.io/Etch-a-Sketch/",
+    image: chiruImg2,
   },
   {
     title: "Tic-Tac-Toe",
@@ -39,11 +41,23 @@ const projects = [
       image: anitaImg1,
   },
   {
-      title: "Library",
-      author: "Anita Kumari",
-      url: "https://anita-kumari1987.github.io/My-Library/",
-      image: anitaImg2,
+      title: "Calculator",
+      author: "Chiranjeevi V",
+      url: "https://chiru1985.github.io/Calculator/",
+      image: chiruImg1,
   },
+  {
+    title: "Restaurant Page",
+    author: "Dylan St. John",
+    url: "https://dstjp.github.io/odin-restaurantpage/",
+    image: dylanImg2,
+  },
+  {
+    title: "Library",
+    author: "Anita Kumari",
+    url: "https://anita-kumari1987.github.io/My-Library/",
+    image: anitaImg2,
+},
 ];
 
 export default function Projects() {
